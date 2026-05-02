4050db2401fbf0795f4792a8a5e0f690c007d019
dblairgonzales-source <dblair.gonzales@gmail.com>
2026-05-02 22:23:12 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,8 +25,8 @@ const primaryLinks = [
     icon: Sparkles,
   },
   {
-    title: 'PRODUCT10N',
-    description: 'Sign up here for PRODCUT10N class bundles. For single class, please proceed to Class Pre-reg',
+    title: 'PRODUCT10N Class Bundles',
+    description: 'For single class, please proceed to Class Pre-reg',
     href: 'https://docs.google.com/forms/d/e/1FAIpQLScTKetr9TM7ZgFsE_OWLPh_fSMELFNF--vZ5cHdbJI8vhGj2A/viewform?usp=dialog',
     icon: Calendar,
   },
